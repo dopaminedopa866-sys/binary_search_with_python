3152676b8daffd3218f828f02db4afbc3a35a552
dopaminedopa866-sys <dopaminedopa866@gmail.com>
2026-04-22 11:04:52 +0600

    binary search algorithm implementation in python

---
+++ binary_search.py
@@ -0,0 +1,18 @@
+arr = [10, 20, 30, 40, 50, 60, 70]
+target = 50
+left = 0
+right = len(arr) - 1
+found = False
+while left <= right:
+    mid = (left + right) // 2
+    if arr[mid] == target:
+        print("Element found at index :", mid)
+        found=True
+        break
+    elif arr[mid] < target:
+        left = mid + 1
+    else:
+        right = mid - 1
+
+if not found:
+    print("Element not found")
